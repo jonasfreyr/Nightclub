0175a5ed4be38b8f739994cf3c7b53053019c571
larah19 <larah19@ru.is>
2022-03-16 18:04:22 +0000

    pull commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float cozy = 70;
+    public float romantic = 50;
+    public float chaotic = 25;
+    public float overall_satisfaction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Reviews.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reviews : MonoBehaviour
+{
+    public float cozy;
+    public float romantic;
+    public float chaotic;
+    public float satisfaction;
+
+    void generateReview(Customer customer) {
+        
+    }
+}

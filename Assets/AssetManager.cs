491d90d3af32f52f9527420b5c054bcbf4738427
gudmunduro <goh00@protonmail.com>
2022-03-17 18:01:15 +0000

    Add remaining features to asset store menu

--- Assets/AssetManager.cs
+++ Assets/AssetManager.cs
@@ -2,52 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Globalization;
 using TMPro;
 using UnityEngine.UI;
 
 public class AssetManager : MonoBehaviour
 {
-    public GameObject Canvas;
-    public Vector2 gridGap;
-    public Vector2 tileSize;
-    public Transform assetStoreItemPrefab;
-    public List<Item> Assets;
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < Assets.Count; i++)
-        {
-            //Instantiate(Assets[0].sprite, Canvas.transform);
-            Debug.Log(Assets.Count);
-            CreateStoreItem(i);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     [Serializable]
     public class Item
     {
         public Sprite sprite;
-        public string Price;
+        public Transform prefab;
+        public string name;
+        public double price;
     }
+    
+    public GameObject assetList;
+    public Vector2 gridGap;
+    public Vector2 tileSize;
+    public int itemsPerRow;
+    public Transform assetStoreItemPrefab;
+    public List<Item> assets;
+    public GameObject assetStoreMenu;
 
-    void CreateStoreItem(int i)
+    // Start is called before the first frame update
+    void Start()
     {
-        var item = Instantiate(assetStoreItemPrefab, Canvas.transform);//Canvas.transform.position + new Vector3(i*3, i*3, 0), Quaternion.identity, Canvas.transform); // instantiate prefab
-        Debug.Log(item);
-        item.Find("AssetStoreItem").GetComponent<Image>().sprite = Assets[i].sprite;
-        item.Find("Price").GetComponent<TextMeshProUGUI>().text = Assets[i].Price;
-        //item.transform.position.x += gridGap.x;
-
+        for (var i = 0; i < assets.Count; i++)
+        {
+            _createStoreItem(i);
+        }
     }
+
+    public void CloseMenu()
+    {
+        assetStoreMenu.SetActive(false);
+    }
+
+    private void _createStoreItem(int i)
+    {
+        var item = Instantiate(assetStoreItemPrefab, assetList.transform);
+        item.Find("Image").GetComponent<Image>().sprite = assets[i].sprite;
+        item.Find("Title").GetComponent<TextMeshProUGUI>().text = assets[i].name;
+        item.Find("Price").GetComponent<TextMeshProUGUI>().text =
+            assets[i].price.ToString("C0", new CultureInfo("en-US", false));
+        item.GetComponent<RectTransform>().anchoredPosition = _gridPositionForIndex(i);
+        item.GetComponent<Button>().onClick.AddListener(() => _createItemFromStore(assets[i]));
+    }
+
+    private void _createItemFromStore(Item item)
+    {
+        Instantiate(item.prefab);
+    }
+
+    private Vector2 _gridPositionForIndex(int index) 
+        => new Vector2((index % itemsPerRow) * (tileSize.x + gridGap.x) + (tileSize.x + gridGap.x) / 2,  -(index/itemsPerRow) * (tileSize.y + gridGap.y) - (tileSize.y + gridGap.y));
 }
 

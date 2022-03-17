c520c8c448dac7da14eea9ab8c570575bd7458be
gudmunduro <goh00@protonmail.com>
2022-03-17 14:12:18 +0000

    Add speaker asset

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public void PlayOnAllAudioSources(AudioClip clip)
+    {
+        var audioSources = GameObject.FindGameObjectsWithTag("AudioSources");
+        foreach (var audioSource in audioSources)
+        {
+            audioSource.GetComponent<AudioSource>().clip = clip;
+        }
+    }
+}

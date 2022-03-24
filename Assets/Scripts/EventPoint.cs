7781a5c703c8bb94d7c02f29cfe8698e0e41f478
Jónas <jonasfb20@ru.is>
2022-03-24 13:47:25 +0000

    Started on event system, only disables the speakers

---
+++ Assets/Scripts/EventPoint.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPoint : MonoBehaviour
+{
+    public virtual void Brake()
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual void Fix()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Assets/Scripts/EventSystem.cs
+++ Assets/Scripts/EventSystem.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class EventSystem : MonoBehaviour
 {
-    
+    public EventPoint[] _eventPoints;
+    private float _timer = 0f;
+    public float interval = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    private void DisableObject()
     {
-        
+        _eventPoints[Random.Range(0, _eventPoints.Length)].Brake();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        _timer += Time.deltaTime;
         
+        
+        if (_timer >= interval)
+        {
+            _timer = 0f;
+
+            DisableObject();
+        }
     }
 }

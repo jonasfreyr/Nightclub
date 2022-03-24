a801bede3cd563ac264dbddf0be526930ee2bd36
gudmunduro <goh00@protonmail.com>
2022-03-24 21:16:12 +0000

    Add eventpoint for puke

---
+++ Assets/Scripts/PukeEvent.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PukeEvent : EventPoint
+{
+    public float cleaningTime;
+    private bool _isBroken;
+    private bool _isFixing;
+
+    void Start()
+    {
+        Break();
+    }
+    
+    public override void Break()
+    {
+        GameManager.Instance.speakersBroken = true;
+
+        _isBroken = true;
+        repairStatusCanvas.SetActive(true);
+    }
+    
+    public override void Fix()
+    {
+        if (_isFixing) return;
+        _isFixing = true;
+        StartCoroutine(_fix());
+    }
+
+    private IEnumerator _fix()
+    {
+        _setRepairButtonState(true);
+        yield return new WaitForSeconds(cleaningTime);
+
+        _isBroken = false;
+        _isFixing = false;
+        _setRepairButtonState(false);
+        repairStatusCanvas.SetActive(false);
+        Destroy(gameObject, 1);
+    }
+
+    public override bool IsBroken()
+    {
+        return _isBroken;
+    }
+}
---
+++ Assets/Scripts/SinkEvent.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkEvent : EventPoint
+{
+    public float fixTime;
+    private bool _isBroken;
+    private bool _isFixing;
+    
+    public override void Break()
+    {
+        GameManager.Instance.speakersBroken = true;
+
+        _isBroken = true;
+        repairStatusCanvas.SetActive(true);
+    }
+    
+    public override void Fix()
+    {
+        if (_isFixing) return;
+        _isFixing = true;
+        StartCoroutine(_fix());
+    }
+
+    private IEnumerator _fix()
+    {
+        _setRepairButtonState(true);
+        yield return new WaitForSeconds(fixTime);
+
+        _isBroken = false;
+        _isFixing = false;
+        repairStatusCanvas.SetActive(false);
+        _setRepairButtonState(false);
+    }
+
+    public override bool IsBroken()
+    {
+        return _isBroken;
+    }
+}

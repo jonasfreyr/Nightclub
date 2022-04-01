3fcc2d3f1d2f11d239b3364203c90013c9fcc15c
Jónas <jonasfb20@ru.is>
2022-04-01 18:26:26 +0000

    Changed the name of scene Jónas3 to main and made the start scene work

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -1,14 +1,11 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Scene = UnityEngine.SceneManagement.Scene;
 
 public class StartScript : MonoBehaviour
 {
-    public GameObject playScene;
-    
     public void LoadScene()
     {
-        SceneManager.LoadScene(playScene.name);
+        SceneManager.LoadScene("Main");
     }
 }
